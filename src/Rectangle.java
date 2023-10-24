15d8fca41f7cae8fa2e8db2dc81683d3bbb3125d
BT_2N2_08 <sample@bths.edu>
2023-10-24 13:54:09 -0400

    Final Commit: 1:53 PM

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Rectangle {
+public class Rectangle  {
+    private int length;
+    public int width;
+
+    public Rectangle(int rectLength, int rectWidth) {
+        length = rectLength;
+        width = rectWidth;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int newLength) {
+        length = newLength;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int newWidth) {
+        width = newWidth;
+    }
+
+    public int area() {
+        int rectArea = width * length;
+        return rectArea;
+    }
+
+    public void printArea() {
+        int area = length * width;
+        System.out.println("The area is " + area);
+    }
 }
+
+
